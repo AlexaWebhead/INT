c4774ad1f11eee5f394dc2d69281b3d1db86fad5
Alexa <alexa@gigmo.co>
2016-04-24 17:14:02 -0700

    styling tweaks for photo section and menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 $(function(){
 	$("#openMenu").click(function(e){
 		e.preventDefault();
+		$(this).toggleClass("active");
 		$(this).children("i").toggleClass('fa-minus fa-bars');
 		$("#headerMenu").toggle(200);
 	})
